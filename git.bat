b88f6ac7f9844c35741e0c96ffbeb5c36c608fec
QGB <QGB@users.noreply.github.com>
2026-08-03 04:46:58 +0800

    git.bat\git_logic.py auto 2026-08-03__04.46.58__.180

--- git.bat
+++ git.bat
@@ -3,9 +3,8 @@ chcp 65001 >nul 2>&1
 cd /d "%~dp0"
 
 :: ===== 基础配置（可按需修改） =====
-set "PY_PATH=D:\test\github\CQ-editor\python.exe"
+set "PY_PATH=C:\QGB\miniforge3\python.exe"
 set "GIT_PATH=C:\QGB\PortableGit\bin\git.exe"
-set "REPO_PATH=775cpu/energetic.git"
 set "BRANCH=master"
 set "SIZE_THRESHOLD=104857600"
 :: ==================================
