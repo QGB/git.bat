40ffb8f97dee239d155100923fca9591b32fc71d
QGB <QGB@users.noreply.github.com>
2026-08-04 14:39:25 +0800

    auto update [max: git_logic.py (34371 bytes)] 2026-08-04__14.39.25__.294

--- git-push.bat
+++ git-push.bat
@@ -1,5 +1,3 @@
-cmd /k "%~dp0git.bat" -v3 -u push git@github.com:QGB/git.bat.git %*
-
-echo %0 %* end.   exit
+cmd /k "%~dp0git.bat" -v3 -u push git@github.com:QGB/git.bat.git %* && echo %0 %* end.   exit
 
 REM pause
